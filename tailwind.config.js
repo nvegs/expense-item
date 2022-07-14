c707ce6f68f9efe87a33c5d764e9a7855b152550
Victory Nnabugwu <Szencskyz13@gmail.com>
2022-07-14 15:14:11 +0100

    adding a toggle button to hide and show the form when clicked

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+       animation: {
+        slide_down: 'slide_down 300ms ease-out forwards',
+      },
+
+      keyframes: {
+
+        slide_down : {
+          '0%': {opacity: 0, transform: 'translateY(-3rem)'},
+          '100%' : {opacity: 1, transform: 'translateY(0)'}, 
+        },
+
+      },
+    },
   },
   plugins: [],
 }
